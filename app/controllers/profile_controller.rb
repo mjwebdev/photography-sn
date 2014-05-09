9979ce404a54087ce9ad5adb9767c8663688191c
Michael <mbaughman929@gmail.com>
2014-05-08 21:55:03 -0700

    Added status validations and profile controller

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,4 +1,11 @@
 class ProfileController < ApplicationController
   def show
+  	@user = User.find_by_profile_name(params[:id])
+  	if @user
+  		@statuses = Status.all
+  		render action: :show
+  	else
+  		render file: 'piublic/404', status: 404, formats: [:html]
+
   end
 end
